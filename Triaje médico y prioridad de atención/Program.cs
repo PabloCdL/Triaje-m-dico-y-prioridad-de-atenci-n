2e790a7c48899638d63a387011b22fbc6c4372e7
PabloCdL <angelcoyoy07@gmail.com>
2026-02-21 18:57:17 -0600

    Defini Variables

--- Triaje médico y prioridad de atención/Program.cs
+++ Triaje médico y prioridad de atención/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Tipo de atención");
+Console.WriteLine("1. Emergencia");
+Console.WriteLine("2. Consulta");
+Console.WriteLine("3. Pediatria");
+Console.WriteLine("4. Traumatologia");
+
+int opciones = int.Parse(Console.ReadLine());
+int edad;
+double temperatura;
+int saturacioO;
+int ndolor;
+double presión;
