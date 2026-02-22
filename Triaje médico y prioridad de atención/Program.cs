1e1a93ae9040017ca8cdc3412ec07df1b60242a5
PabloCdL <angelcoyoy07@gmail.com>
2026-02-21 22:57:48 -0600

    Ingrese los datos y ordene los casos del switch

--- Triaje médico y prioridad de atención/Program.cs
+++ Triaje médico y prioridad de atención/Program.cs
@@ -9,4 +9,61 @@ int edad;
 double temperatura;
 int saturacioO;
 int ndolor;
-double presión;
+double presion;
+
+switch (opciones)
+{
+    case 1:
+        Console.WriteLine("Ingrese la edad");
+        edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la temperatura");
+        temperatura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la saturacion del oxigeno");
+        saturacioO = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el nivel de dolor (escala del 1-10)");
+        ndolor = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la presion sistolica");
+        presion = double.Parse(Console.ReadLine());
+
+    break;
+    case 2:
+        Console.WriteLine("Ingrese la edad");
+        edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la temperatura");
+        temperatura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la saturacion del oxigeno");
+        saturacioO = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el nivel de dolor (escala del 1-10)");
+        ndolor = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la presion sistolica");
+        presion = double.Parse(Console.ReadLine());
+        break;
+    case 3:
+        Console.WriteLine("Ingrese la edad");
+        edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la temperatura");
+        temperatura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la saturacion del oxigeno");
+        saturacioO = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el nivel de dolor (escala del 1-10)");
+        ndolor = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la presion sistolica");
+        presion = double.Parse(Console.ReadLine());
+        break;
+    case 4:
+        Console.WriteLine("Ingrese la edad");
+        edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la temperatura");
+        temperatura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la saturacion del oxigeno");
+        saturacioO = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el nivel de dolor (escala del 1-10)");
+        ndolor = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la presion sistolica");
+        presion = double.Parse(Console.ReadLine());
+        break;
+
+    default:
+        Console.WriteLine("Desicion no valida");
+        break;
+}
